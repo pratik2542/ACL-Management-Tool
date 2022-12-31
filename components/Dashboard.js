05aac4b49a737a2ee541adf8ae0ca32c035a9124
Pratiksinh Makwana <pratikmak2542@gmail.com>
2022-12-31 12:54:15 +0530

    update dashboard before connect

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -4,6 +4,13 @@ import { useAccount } from "wagmi";
 import { createClient } from "urql";
 import { getTransactionDescription } from "@superfluid-finance/sdk-core";
 import ConnectWalletCustom from "./ConnectWalletCustom";
+import gif from "../public/stream-loop.gif";
+import avatar1 from "../public/avatar-image.gif";
+import avatar2 from "../public/avatar2.png";
+import avatar3 from "../public/avatar3.png";
+
+import Image from "next/image";
+
 import { sign } from "crypto";
 import { ethers } from "ethers";
 import { Framework } from "@superfluid-finance/sdk-core";
@@ -512,35 +519,164 @@ function Dashboard() {
       <div className="db-sub">
         <h1>Connect to Superfluid</h1>
         <p>Connect your wallet, view any wallet, or take a look around!</p>
-        <div className="db-grid-sub">
+        <div className="db-grid-sub w-[70%] mx-auto my-0">
           <div className="grid-sub min-h-[120px]">
             <span className="grid-sub-title">Set Permissions</span>
             <span className="grid-sub-info">Update Operator Permissions</span>
+            <div className="flex items-center justify-center mt-4">
+              <div className="shadow-[#cccccc40] shadow-[0px_0px_6px_3px_rgb(204,204,204,0.25)] flex items-center justify-between w-30 px-1 py-1 rounded-xl">
+                <Image
+                  src={avatar1}
+                  alt="send stream image"
+                  className="w-8 p-1 rounded-xl"
+                />
+                <div className="flex flex-col w-20 rounded-xl p-1 ">
+                  <div className="bg-[#12141e1c] h-2 w-full content-none mb-1 rounded-lg"></div>
+                  <div className="bg-[#12141e1c] h-2 w-1/2 content-none rounded-lg"></div>
+                </div>
+              </div>
+              <span className="p-[6px] bg-[#10bb3514] rounded-xl mx-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  enableBackground="new 0 0 24 24"
+                  height="24px"
+                  viewBox="0 0 24 24"
+                  width="24px"
+                  className="fill-[#10bb35]"
+                >
+                  <g>
+                    <rect fill="none" height="24" width="24" />
+                    <rect fill="none" height="24" width="24" />
+                  </g>
+                  <g>
+                    <g>
+                      <path
+                        d="M20,3H4C2.9,3,2,3.9,2,5v14c0,1.1,0.9,2,2,2h16c1.1,0,2-0.9,2-2V5 C22,3.9,21.1,3,20,3z M9,17H6c-0.55,0-1-0.45-1-1c0-0.55,0.45-1,1-1h3c0.55,0,1,0.45,1,1C10,16.55,9.55,17,9,17z M9,13H6 c-0.55,0-1-0.45-1-1c0-0.55,0.45-1,1-1h3c0.55,0,1,0.45,1,1C10,12.55,9.55,13,9,13z M9,9H6C5.45,9,5,8.55,5,8c0-0.55,0.45-1,1-1h3 c0.55,0,1,0.45,1,1C10,8.55,9.55,9,9,9z M18.7,11.12l-3.17,3.17c-0.39,0.39-1.03,0.39-1.42,0l-1.41-1.42 c-0.39-0.39-0.39-1.02,0-1.41c0.39-0.39,1.02-0.39,1.41,0l0.71,0.71l2.47-2.47c0.39-0.39,1.02-0.39,1.41,0l0.01,0.01 C19.09,10.1,19.09,10.74,18.7,11.12z"
+                        fill-rule="evenodd"
+                      />
+                    </g>
+                  </g>
+                </svg>
+              </span>
+              <div className=" shadow-[#cccccc40] shadow-xl flex items-center justify-between w-30 px-1 py-1 rounded-xl">
+                <Image
+                  src={avatar2}
+                  alt="send stream image"
+                  className="w-8 p-1 rounded-xl"
+                />
+                <div className="flex flex-col w-20 rounded-xl p-1 ">
+                  <div className="bg-[#12141e1c] h-2 w-full content-none mb-1 rounded-xl"></div>
+                  <div className="bg-[#12141e1c] h-2 w-1/2 content-none rounded-xl"></div>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="grid-sub min-h-[120px]">
             <span className="grid-sub-title">Authorize Full Control</span>
             <span className="grid-sub-info">
               Grant Full Operator Permissions to an Account
             </span>
+            <div className="flex items-center justify-center mt-4">
+              <span className="mr-2 p-[6px] bg-[#10bb3514] rounded-xl">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  enableBackground="new 0 0 24 24"
+                  height="24px"
+                  viewBox="0 0 24 24"
+                  width="24px"
+                  className="fill-[#10bb35]"
+                >
+                  <g>
+                    <rect fill="none" height="24" width="24" />
+                    <rect fill="none" height="24" width="24" />
+                  </g>
+                  <g>
+                    <path d="M23,12l-2.44-2.79l0.34-3.69l-3.61-0.82L15.4,1.5L12,2.96L8.6,1.5L6.71,4.69L3.1,5.5L3.44,9.2L1,12l2.44,2.79l-0.34,3.7 l3.61,0.82L8.6,22.5l3.4-1.47l3.4,1.46l1.89-3.19l3.61-0.82l-0.34-3.69L23,12z M9.38,16.01L7,13.61c-0.39-0.39-0.39-1.02,0-1.41 l0.07-0.07c0.39-0.39,1.03-0.39,1.42,0l1.61,1.62l5.15-5.16c0.39-0.39,1.03-0.39,1.42,0l0.07,0.07c0.39,0.39,0.39,1.02,0,1.41 l-5.92,5.94C10.41,16.4,9.78,16.4,9.38,16.01z" />
+                  </g>
+                </svg>
+              </span>
+              <div className=" shadow-[#cccccc40] shadow-xl flex items-center justify-between w-30 px-1 py-1 rounded-xl">
+                <Image
+                  src={avatar2}
+                  alt="send stream image"
+                  className="w-8 p-1 rounded-xl"
+                />
+                <div className="flex flex-col w-20 rounded-xl p-1 ">
+                  <div className="bg-[#12141e1c] h-2 w-full content-none mb-1 rounded-xl"></div>
+                  <div className="bg-[#12141e1c] h-2 w-1/2 content-none rounded-xl"></div>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="grid-sub min-h-[120px]">
             <span className="grid-sub-title">Revoke Full Control</span>
             <span className="grid-sub-info">
               Revoke Full Operator Permissions to an Account
             </span>
+            <div className="flex items-center justify-center mt-4">
+              <span className="mr-2 p-[6px] bg-[#10bb3514] rounded-xl">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="24px"
+                  viewBox="0 0 24 24"
+                  width="24px"
+                  className="fill-[#d22525]"
+                >
+                  <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8 0-1.85.63-3.55 1.69-4.9L16.9 18.31C15.55 19.37 13.85 20 12 20zm6.31-3.1L7.1 5.69C8.45 4.63 10.15 4 12 4c4.42 0 8 3.58 8 8 0 1.85-.63 3.55-1.69 4.9z" />
+                </svg>
+              </span>
+              <div className=" shadow-[#cccccc40] shadow-xl flex justify-between w-30 px-1 py-1 rounded-xl">
+                <Image
+                  src={avatar3}
+                  alt="send stream image"
+                  className="w-8 p-1 rounded-xl"
+                />
+                <div className="flex flex-col w-20 rounded-xl p-1 ">
+                  <div className="bg-[#12141e1c] h-2 w-full content-none mb-1 rounded-xl"></div>
+                  <div className="bg-[#12141e1c] h-2 w-1/2 content-none rounded-xl"></div>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="grid-sub min-h-[120px]">
             <span className="grid-sub-title">Send a Stream</span>
             <span className="grid-sub-info">
               Pick a Sender, Recipient, Token and Network
             </span>
+            <div className="flex items-center justify-center mt-4">
+              <div className="shadow-[#cccccc40] shadow-xl flex justify-between w-30 px-1 py-1 rounded-xl">
+                <Image
+                  src={avatar1}
+                  alt="send stream image"
+                  className="w-8 p-1 rounded-xl"
+                />
+                <div className="flex flex-col w-20 rounded-xl p-1 ">
+                  <div className="bg-[#12141e1c] h-2 w-full content-none mb-1 rounded-xl"></div>
+                  <div className="bg-[#12141e1c] h-2 w-1/2 content-none rounded-xl"></div>
+                </div>
+              </div>
+              <span>
+                <Image src={gif} alt="send stream image" className="w-8" />
+              </span>
+              <div className=" shadow-[#cccccc40] shadow-xl flex justify-between w-30 px-1 py-1 rounded-xl">
+                <Image
+                  src={avatar2}
+                  alt="send stream image"
+                  className="w-8 p-1 rounded-xl"
+                />
+                <div className="flex flex-col w-20 rounded-xl p-1 ">
+                  <div className="bg-[#12141e1c] h-2 w-full content-none mb-1 rounded-xl"></div>
+                  <div className="bg-[#12141e1c] h-2 w-1/2 content-none rounded-xl"></div>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="grid-sub min-h-[120px]">
             <span className="grid-sub-title">Modify and Cancle Streams</span>
             <span className="grid-sub-info">
               Update Flow Rate and Delete a Stream using Operator Control
             </span>
-            <div className="edit-delete flex items-center justify-center mt-4">
+            <div className="flex items-center justify-center mt-4">
               <button className="p-[6px] bg-[#10bb3514] rounded-xl">
                 <svg
                   className="fill-[#10bb35] w-6"
@@ -570,6 +706,40 @@ function Dashboard() {
             <span className="grid-sub-info">
               View All Addresses to whom permissions are given
             </span>
+            <div className="flex items-center justify-center mt-4 px-2">
+              <span className="mr-2 p-[6px] bg-[#10bb3514] rounded-xl">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  enableBackground="new 0 0 24 24"
+                  height="24px"
+                  viewBox="0 0 24 24"
+                  width="24px"
+                  className="fill-[#10bb35]"
+                >
+                  <g>
+                    <rect fill="none" height="24" width="24" />
+                    <rect fill="none" height="24" width="24" />
+                  </g>
+                  <g>
+                    <g>
+                      <path d="M17,11c0.34,0,0.67,0.04,1,0.09V7.58c0-0.8-0.47-1.52-1.2-1.83l-5.5-2.4c-0.51-0.22-1.09-0.22-1.6,0l-5.5,2.4 C3.47,6.07,3,6.79,3,7.58v3.6c0,4.54,3.2,8.79,7.5,9.82c0.55-0.13,1.08-0.32,1.6-0.55C11.41,19.47,11,18.28,11,17 C11,13.69,13.69,11,17,11z" />
+                      <path d="M17,13c-2.21,0-4,1.79-4,4c0,2.21,1.79,4,4,4s4-1.79,4-4C21,14.79,19.21,13,17,13z M17,14.38c0.62,0,1.12,0.51,1.12,1.12 s-0.51,1.12-1.12,1.12s-1.12-0.51-1.12-1.12S16.38,14.38,17,14.38z M17,19.75c-0.93,0-1.74-0.46-2.24-1.17 c0.05-0.72,1.51-1.08,2.24-1.08s2.19,0.36,2.24,1.08C18.74,19.29,17.93,19.75,17,19.75z" />
+                    </g>
+                  </g>
+                </svg>
+              </span>
+              <div className=" shadow-[#cccccc40] shadow-xl flex items-center justify-between w-25 px-1 py-1 rounded-xl">
+                <Image
+                  src={avatar2}
+                  alt="send stream image"
+                  className="w-6 p-1 rounded-xl"
+                />
+                <div className="flex flex-col w-20 rounded-xl p-1 ">
+                  <div className="bg-[#12141e1c] h-2 w-full content-none rounded-xl mb-1"></div>
+                  <div className="bg-[#12141e1c] h-2 w-1/2 content-none rounded-xl"></div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div className="custom-wallet w-80 mx-auto my-8">
